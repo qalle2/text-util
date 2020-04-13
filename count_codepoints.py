cabbad261c1c25cbf25487646b617433b1f41f67
Kalle <qalle85@gmail.com>
2020-04-13 09:39:53 +0300

    remove features

--- count_codepoints.py
+++ count_codepoints.py
@@ -1,69 +1,31 @@
-"""Count Unicode codepoints in a text file."""
+"""Count Unicode codepoints from stdin. Output them in CSV format, sorted by codepoint.
+See https://unicode.org/reports/tr44/#Property_Values for Unicode general category values."""
 
-import argparse
 import collections
-import os
 import sys
 import unicodedata
 
-def parse_arguments():
-    """Parse command line arguments using argparse."""
+def count_characters_from_stdin():
+    """Count characters from stdin."""
 
-    parser = argparse.ArgumentParser(
-        description="Count Unicode codepoints in a text file. Output them in CSV format, sorted by "
-        "codepoint.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        epilog="See https://unicode.org/reports/tr44/#Property_Values for general category values."
-    )
-
-    parser.add_argument(
-        "-n", "--normalization", choices=("", "nfd", "nfc", "nfkd", "nfkc"), default="",
-        help="Which Unicode Normalization Form to apply to each line (\"\"=none). See "
-        "https://unicode.org/reports/tr15/#Norm_Forms"
-    )
-    parser.add_argument("input_file", help="The UTF-8 text file to read.")
-
-    args = parser.parse_args()
-    if not os.path.isfile(args.input_file):
-        sys.exit("Input file not found.")
-    return args
-
-def count_codepoints(handle, settings):
-    """Count codepoints in the file."""
-
-    handle.seek(0)
-    codepointCounts = collections.Counter()
-    for line in handle:
-        if settings.normalization:
-            line = unicodedata.normalize(settings.normalization.upper(), line)
-        codepointCounts.update(ord(char) for char in line)
-    return codepointCounts
-
-def print_codepoints(codepointCounts):
-    """Print codepoints in CSV format."""
-
-    print('"codepoint","name","general category","count"')
-    for codepoint in sorted(codepointCounts):
-        char = chr(codepoint)
-        print('{:d},"{:s}","{:s}",{:d}'.format(
-            codepoint,
-            unicodedata.name(char, "(unknown)"),
-            unicodedata.category(char),
-            codepointCounts[codepoint]
-        ))
+    chars = collections.Counter()
+    for line in sys.stdin:
+        line = line[:-1] if line.endswith("\n") else line
+        chars.update(char for char in line)
+    return chars
 
 def main():
     """The main function."""
 
-    settings = parse_arguments()
-    try:
-        with open(settings.input_file, "rt", encoding="utf8") as handle:
-            codepointCounts = count_codepoints(handle, settings)
-    except UnicodeDecodeError:
-        sys.exit("Error: the file is not a UTF-8 text file.")
-    except OSError:
-        sys.exit("Error reading the file.")
-    print_codepoints(codepointCounts)
+    chars = count_characters_from_stdin()
+    print('"codepoint","name","general category","count"')
+    for char in sorted(chars):
+        print('{:d},"{:s}","{:s}",{:d}'.format(
+            ord(char),
+            unicodedata.name(char, "(unknown)"),
+            unicodedata.category(char),
+            chars[char]
+        ))
 
 if __name__ == "__main__":
     main()
