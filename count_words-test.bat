604a1bd8702480a379f63afd61a1c0adcda1ce59
Kalle <qalle85@gmail.com>
2020-04-13 22:30:13 +0300

    Update count_words-test.bat

--- count_words-test.bat
+++ count_words-test.bat
@@ -5,21 +5,9 @@ echo === Default settings ===
 python count_words.py < test-in\count_words.txt
 echo.
 
-echo === No counts ===
-python count_words.py --no-counts < test-in\count_words.txt
-echo.
-
-echo === Input from casefold.py, default settings ===
+echo === Input from casefold.py ===
 python casefold.py < test-in\count_words.txt | python count_words.py
-echo.
-
-echo === Input from casefold.py, no counts ===
-python casefold.py < test-in\count_words.txt | python count_words.py --no-counts
 echo.
 
 echo === Binary file ===
 python count_words.py < test-in\allbytes.dat
-echo.
-
-echo === Help text ===
-python count_words.py --help
