c3629910e6a3b4f9764e7b89f07f8275c4329a20
Kalle <qalle85@gmail.com>
2020-04-13 22:30:10 +0300

    remove --no-counts option

--- count_words.py
+++ count_words.py
@@ -1,23 +1,9 @@
-"""Count words (sequences of Unicode Letter characters) from stdin."""
+"""Count words (sequences of Unicode Letter characters) from stdin. Output distinct words and their
+counts in CSV format in Unicode order."""
 
-import argparse
 import collections
 import sys
 import unicodedata
-
-def parse_arguments():
-    """Parse command line options and arguments with argparse."""
-
-    parser = argparse.ArgumentParser(
-        description="Count words (sequences of Unicode Letter characters) from stdin. Output "
-        "distinct words and their counts in CSV format in Unicode order."
-    )
-
-    parser.add_argument(
-        "-c", "--no-counts", action="store_true", help="Do not print the word counts."
-    )
-
-    return parser.parse_args()
 
 def string_to_words(string_):
     """Generate words (sequences of Unicode Letter characters) from a string."""
@@ -46,15 +32,9 @@ def stdin_to_words():
 def main():
     """The main function."""
 
-    args = parse_arguments()
-    words = stdin_to_words()
-    if args.no_counts:
-        for word in sorted(set(words)):
-            print(word)
-    else:
-        words = dict(collections.Counter(words))
-        for word in sorted(words):
-            print(f'"{word:s}",{words[word]:d}')
+    words = dict(collections.Counter(stdin_to_words()))
+    for word in sorted(words):
+        print(f'"{word:s}",{words[word]:d}')
 
 if __name__ == "__main__":
     main()
